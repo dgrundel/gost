ae07750faad58f6d5078f7f6e21d23f1f6c4d3b1
Daniel <daniel@grundel.local>
2025-03-09 14:57:34 -0700

    expanding conditional block type

--- parser/nodes/conditionalExpression.go
+++ parser/nodes/conditionalExpression.go
@@ -1,24 +1,65 @@
 package nodes
 
-import "io"
+import (
+	"bytes"
+	"io"
+)
 
 type ConditionalExpression interface {
 	Node
+	SetPrev(ConditionalExpression)
+	SetNext(ConditionalExpression)
 }
 
 type conditionalExpression struct {
 	node
-	condition func(RenderContext) bool
+	condition Condition
+	prev      ConditionalExpression
 	next      ConditionalExpression
 }
 
-func NewConditionalExpression() ConditionalExpression {
-	return &conditionalExpression{}
+func NewConditionalExpression(condition Condition) ConditionalExpression {
+	return &conditionalExpression{
+		condition: condition,
+	}
+}
+
+func (e *conditionalExpression) SetPrev(prev ConditionalExpression) {
+	e.prev = prev
+}
+
+func (e *conditionalExpression) SetNext(next ConditionalExpression) {
+	e.next = next
+}
+
+func (e *conditionalExpression) OuterHTML() string {
+	var buf bytes.Buffer
+
+	if e.prev == nil && e.condition != nil {
+		buf.WriteString("{if ")
+		buf.WriteString(e.condition.OuterHTML())
+		buf.WriteString("} ")
+	} else if e.prev != nil && e.condition != nil {
+		buf.WriteString("{else if ")
+		buf.WriteString(e.condition.OuterHTML())
+		buf.WriteString("} ")
+	} else if e.prev != nil && e.condition == nil {
+		buf.WriteString("{else} ")
+	} else {
+		buf.WriteString("{if true} ")
+	}
+
+	for _, child := range e.children {
+		buf.WriteString(child.OuterHTML())
+	}
+
+	buf.WriteString("{/if}")
+	return buf.String()
 }
 
 func (e *conditionalExpression) Render(c RenderContext, w io.Writer) error {
 	// nil condition expected in else branch
-	if e.condition == nil || e.condition(c) {
+	if e.condition == nil || e.condition.Evaluate(c) {
 		for _, child := range e.children {
 			err := child.Render(c, w)
 			if err != nil {
@@ -29,4 +70,25 @@ func (e *conditionalExpression) Render(c RenderContext, w io.Writer) error {
 		return e.next.Render(c, w)
 	}
 	return nil
+}
+
+type Condition interface {
+	Evaluate(RenderContext) bool
+	OuterHTML() string
+}
+
+type stringCondition struct {
+	expression string
+}
+
+func NewStringCondition(expression string) Condition {
+	return &stringCondition{expression: expression}
+}
+
+func (s *stringCondition) Evaluate(c RenderContext) bool {
+	return true
+}
+
+func (s *stringCondition) OuterHTML() string {
+	return s.expression
 }
